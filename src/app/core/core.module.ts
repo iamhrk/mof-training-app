2ddb3d81bc26be9692b8c600fb6ee6620f998c92
iamhrk <iamhrk8@gmail.com>
2025-05-01 18:40:12 +0530

    Load Header Component on AppComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -9,7 +9,11 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
